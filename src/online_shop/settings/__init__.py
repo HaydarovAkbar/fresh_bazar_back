b3a13b4c09ef71c987d35bb87b0faeb2f2ab1dd9
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-05-25 12:24:37 +0500

    local production

---
+++ src/online_shop/settings/__init__.py
@@ -0,0 +1,14 @@
+from distutils.command.config import config
+
+from .base import *
+
+env_type = config('ENV_NAME')
+
+if env_type == 'production':
+    from .production import *
+elif env_type == 'local':
+    from .local import *
+elif env_type == 'staging':
+    from .staging import *
+else:
+    print('No environment chosen!')
