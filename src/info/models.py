a4b7ea89acb3c4459c6c1d493d52962d5d42ab25
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-06-17 12:27:53 +0500

    add info database design pattern and requirements

---
+++ src/info/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    abbreviation = models.CharField(max_length=2)
+    date_of_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    shortname = models.CharField(max_length=20)
+    fullname = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    postal_code = models.CharField(max_length=10)
+    date_of_created = models.DateTimeField(auto_now_add=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.shortname
+
+
+class Region(models.Model):
+    shortname = models.CharField(max_length=50)
+    fullname = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    date_of_created = models.DateTimeField(auto_now_add=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.shortname
+
+
+class District(models.Model):
+    shortname = models.CharField(max_length=50)
+    fullname = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    date_of_created = models.DateTimeField(auto_now_add=True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.shortname
--- src/online_shop/settings/__init__.py
+++ src/online_shop/settings/__init__.py
@@ -1,12 +1,10 @@
-from distutils.command.config import config
-
+from decouple import config
 from .base import *
 
 env_type = config('ENV_NAME')
-
 if env_type == 'production':
     from .production import *
-elif env_type == 'local':
+elif env_type == 'localhost':
     from .local import *
 elif env_type == 'staging':
     from .staging import *
