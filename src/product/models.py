3efca2e8b917280a4ea8e363426082ebcc9729b4
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-06-17 14:53:42 +0500

    product add to product models.py

--- src/product/models.py
+++ src/product/models.py
@@ -26,6 +26,14 @@ class ProductCategory(models.Model):
         self.save()
         return True
 
+    @property
+    def get_image_url(self):
+        # "Returns the image url."
+        try:
+            return '%s%s' % (settings.HOST, self.image_url.url)
+        except ValueError:
+            return ''
+
 
 class ProductInventory(models.Model):
     quantity = models.IntegerField(_("Product Inventory Quantity"), default=0)
@@ -75,3 +83,40 @@ class Discount(models.Model):
         self.deleted_at = timezone.now()
         self.save()
         return True
+
+
+class Product(models.Model):
+    name = models.CharField(_("Product Name"), max_length=50)
+    description = models.TextField(_("Product Description"), null=True, blank=True)
+    price = models.DecimalField(_("Product Price"), max_digits=10, decimal_places=2)
+    sku = models.CharField(_("Product SKU"), max_length=50)
+    date_of_created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    image_url = models.URLField(_("Product Image URL"), upload_to="static/images/product", null=True)
+    product_category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    product_inventory = models.ForeignKey(ProductInventory, on_delete=models.CASCADE, null=True)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, null=True)
+    state = models.ForeignKey("info.State", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+        ordering = ("id",)
+        db_table = "product"
+
+    def delete(self, *args):
+        self.deleted_at = timezone.now()
+        self.save()
+        return True
+
+    @property
+    def get_image_url(self):
+        # "Returns the image url."
+        try:
+            return '%s%s' % (settings.HOST, self.image_url.url)
+        except ValueError:
+            return ''
