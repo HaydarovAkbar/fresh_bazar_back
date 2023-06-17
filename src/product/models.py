28ecb497b684d1b2604d8343a30279694315b7bc
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-06-17 14:58:07 +0500

    code realise

--- src/product/models.py
+++ src/product/models.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 
 class ProductCategory(models.Model):
