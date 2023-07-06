d02d0ab238ae5c7871143f23c28c2ecf5c411ba1
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-07-06 15:53:29 +0500

    category/serializers.py change
    and migrate database

--- src/api/apps/category/serializers.py
+++ src/api/apps/category/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import category
+from api.models import category
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
@@ -7,21 +7,13 @@ class ProductCategorySerializer(serializers.ModelSerializer):
         model = category.ProductCategory
         fields = '__all__'
 
-    def create(self, validated_data):
-        validated_data['state_id'] = self.context['request'].data.get('state')
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        validated_data['state_id'] = self.context['request'].data.get('state')
-        return super().update(instance, validated_data)
-
     def to_representation(self, instance):
         return {
             'id': instance.id,
             'name': instance.name,
             'description': instance.description,
-            'state': instance.state.name,
-            'state_id': instance.state.id,
+            'state_name': instance.state.name,
+            'state': instance.state.id,
             'date_of_created': instance.date_of_created,
             'updated_at': instance.updated_at,
         }
