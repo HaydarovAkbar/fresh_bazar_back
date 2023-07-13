62175c8f6315dcb95d7714f3d5078e3bace4357b
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-07-13 19:52:58 +0500

    change code

--- src/api/apps/users/serializers.py
+++ src/api/apps/users/serializers.py
@@ -12,10 +12,37 @@ class UserSerializers(serializers.ModelSerializer):
             'id', 'username', 'email', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined', 'groups',
             'user_permissions',)
 
-    def create(self, validated_data):
-        validated_data['state_id'] = self.context['request'].data.get('state')
-        return super().create(validated_data)
+
+class UserSerializersCreate(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'last_login',
+                  'date_joined', 'groups', 'user_permissions',)
+        extra_kwargs = {'password': {'write_only': True}}
+
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(**validated_data)
+    #     return user
+
+
+class UserSerializersUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'last_login',
+                  'date_joined', 'groups', 'user_permissions',)
+        extra_kwargs = {'password': {'write_only': True}}
 
     def update(self, instance, validated_data):
-        validated_data['state_id'] = self.context['request'].data.get('state')
-        return super().update(instance, validated_data)
+        user = User.objects.get(id=instance.id)
+        user.username = validated_data.get('username', user.username)
+        user.email = validated_data.get('email', user.email)
+        user.is_staff = validated_data.get('is_staff', user.is_staff)
+        user.is_active = validated_data.get('is_active', user.is_active)
+        user.is_superuser = validated_data.get('is_superuser', user.is_superuser)
+        user.last_login = validated_data.get('last_login', user.last_login)
+        user.date_joined = validated_data.get('date_joined', user.date_joined)
+        user.groups = validated_data.get('groups', user.groups)
+        user.user_permissions = validated_data.get('user_permissions', user.user_permissions)
+        user.save()
+        return user
+
--- src/api/apps/users/views.py
+++ src/api/apps/users/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
-from .serializers import UserSerializers
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from .serializers import UserSerializers, UserSerializersCreate
 from api.filters import UserFilter
 from api.pagination import DefaultPagination
-
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from cryptography.fernet import Fernet
-from rest_framework.response import Response
 
 
 def encrypt(message):
@@ -25,6 +25,37 @@ def decrypt(key, message):
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializers
+    serializer_class = UserSerializersCreate
     pagination_class = DefaultPagination
     filter_backends = [DjangoFilterBackend, SearchFilter]
+    parser_classes = (MultiPartParser,)
+
+    def encrypt_data(self, data, encryption_key):
+        cipher_suite = Fernet(encryption_key)
+        encrypted_data = cipher_suite.encrypt(data.encode())
+        return encrypted_data
+
+    def decrypt_data(self, encrypted_data, encryption_key):
+        cipher_suite = Fernet(encryption_key)
+        decrypted_data = cipher_suite.decrypt(encrypted_data)
+        return decrypted_data.decode()
+
+    # def create(self, request, *args, **kwargs):
+    #     data = request.data
+    #     data['password'], key = encrypt(data['password'])
+    #     data['key'] = key
+    #     serializer = self.get_serializer(data=data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(is_superuser=False)
+        serializer = UserSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = UserSerializers(instance)
+        return Response(serializer.data)
